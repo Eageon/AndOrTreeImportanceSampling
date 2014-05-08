3c73e60ba7a3b16caf70cd12ebb07e8a8fe717fa
Eageon <yujun.eageon@gmail.com>
2014-05-08 03:19:39 -0500

    sampling

---
+++ src/AndOrSampling.java
@@ -0,0 +1,134 @@
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class AndOrSampling {
+
+	LinkedList<int[]> cachedSamples;
+	ArrayList<OrNode> orNodes;
+	OrNode root;
+	
+	public double startSampling(GraphicalModel model, int w, int N) {
+		
+		ArrayList<Variable> nonSoftEvidenceVariables = new ArrayList<>();
+		for (Variable variable : model.nonEvidenceVars) {
+			if(!variable.isEvidence) {
+				nonSoftEvidenceVariables.add(variable);
+			}
+		}
+		UniformSampler Q = new UniformSampler(nonSoftEvidenceVariables);
+		Q.model = model;
+		
+		for (int i = 0; i < N; i++) {
+			Q.sample();
+			cachedSamples.add(Q.dumpSampleInclusive());
+		}
+		
+		return postOrderTraverse(root);
+	}
+	
+	public double postOrderTraverse() {
+		return postOrderTraverse(root);
+	}
+	
+	public double postOrderTraverse(OrNode orNode) {
+		// virtual And Node
+		//AndNode andNode = new AndNode(0);
+		//andNode.parent = orNode;
+		//andNode.children = orNode.children;
+		double vOr = 0.0;
+		int totalPresentSamples = 0;
+		int[] andNodePresentSamples = new int[orNode.nodeVariable.domainSize()];
+		for (int i = 0; i < orNode.nodeVariable.domainSize(); i++) {
+			for (int[] sampleVals : cachedSamples) {
+				if(sampleVals[orNode.nodeVariable.index] == i) {
+					// present sample;
+					totalPresentSamples++;
+					andNodePresentSamples[i]++;
+				}
+			}	
+		}
+		
+		if(totalPresentSamples == 0) {
+			return 0.0;
+		}
+	
+		for (int i = 0; i < orNode.nodeVariable.domainSize(); i++) {
+			// Simulating And node that is the child of orNode
+			orNode.value = i; 
+			// caculate w for xi
+			double w = 0.0;
+			double vAnd = 1.0;
+			for (int j = 0; j < orNode.children.size(); j++) {
+				OrNode secondOrNode = (OrNode) orNode.children.get(j);
+				vAnd *= postOrderTraverse(secondOrNode);
+			}
+			
+			vOr += (andNodePresentSamples[i] * w * vAnd) / totalPresentSamples;
+		}
+		
+		return vOr;
+	}
+
+	public static void usage() {
+		System.out.println("java  ImportanceSampling " + "FILENAME " + "w "
+				+ "N");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		if (3 > args.length) {
+			usage();
+			System.exit(0);
+		}
+
+		String fileName = args[0];
+		int w = Integer.valueOf(args[1]);
+		int N = Integer.valueOf(args[2]);
+		boolean isAdaptive = (args[3].equals("adaptive")) ? true : false;
+
+		long startTime = System.currentTimeMillis();
+
+		try {
+			PrintStream writer = new PrintStream(fileName + ".output." + w
+					+ "." + N + "" + args[3]);
+			GraphicalModel model = new GraphicalModel(fileName);
+			writer.println("Network data loading completed: "
+					+ model.variables.size() + " variables, "
+					+ model.factors.size() + " factors");
+			writer.println(model.network + " network");
+			model.readSoftEvidence(fileName + ".evid");
+
+			writer.println("Evidence loaded, and variables instantiation completed. "
+					+ model.evidenceCount + " evidence");
+
+			ImportanceSampling sampling = new ImportanceSampling();
+			double result = sampling.startSampling(model, w, N, isAdaptive);
+
+			writer.println("Elimination completed");
+			writer.println("");
+			writer.println("====================RESULT========================");
+			if (model.network.equals("MARKOV")) {
+				writer.println("Z = " + result);
+			} else {
+				writer.println("The probability of evidence = " + result);
+				writer.println("");
+				System.out.println("Empty Factor Count = "
+						+ model.emptyFactorCount);
+				System.out.println("probe = " + model.probe);
+			}
+
+			long endTime = System.currentTimeMillis();
+			writer.println("Running Time = " + (double) (endTime - startTime)
+					/ 1000 + "secs");
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Succeed!");
+		System.out.println("Output file is " + fileName + ".output");
+	}
+
+}
--- src/PseudoTree.java
+++ src/PseudoTree.java
@@ -12,7 +12,7 @@ public class PseudoTree {
 		this.model = model;
 	}
 	
-	public void generatePseudoTree() {
+	public OrNode generatePseudoTree() {
 		ArrayList<Integer> softOrder = model.computeSoftOrder();
 		ArrayList<ArrayList<Factor>> clusters = model.generateSoftClusters(softOrder);
 		ArrayList<Variable> order = model.orderIntToOrderVar(softOrder);
@@ -52,6 +52,7 @@ public class PseudoTree {
 				}
 			}
 		}
+		return root;
 	}
 	
 	public int treeHeight() {
@@ -128,48 +129,7 @@ public class PseudoTree {
 		}
 	}
 	
-	public double postOrderTraverse() {
-		return 0.0;
-	}
 	
-	public double postOrderTraverse(OrNode orNode) {
-		// virtual And Node
-		//AndNode andNode = new AndNode(0);
-		//andNode.parent = orNode;
-		//andNode.children = orNode.children;
-		double vOr = 0.0;
-		int totalPresentSamples = 0;
-		int[] andNodePresentSamples = new int[orNode.nodeVariable.domainSize()];
-		for (int i = 0; i < orNode.nodeVariable.domainSize(); i++) {
-			for (int[] sampleVals : cachedSamples) {
-				if(sampleVals[orNode.nodeVariable.index] == i) {
-					// present sample;
-					totalPresentSamples++;
-					andNodePresentSamples[i]++;
-				}
-			}	
-		}
-		
-		if(totalPresentSamples == 0) {
-			return 0.0;
-		}
-		
-		for (int i = 0; i < orNode.nodeVariable.domainSize(); i++) {
-			// Simulating And node that is the child of orNode
-			orNode.value = i; 
-			// caculate w for xi
-			double w = 0.0;
-			double vAnd = 1.0;
-			for (int j = 0; j < orNode.children.size(); j++) {
-				OrNode secondOrNode = (OrNode) orNode.children.get(j);
-				vAnd *= postOrderTraverse(secondOrNode);
-			}
-			
-			vOr += (andNodePresentSamples[i] * w * vAnd) / totalPresentSamples;
-		}
-		
-		return vOr;
-	}
 	
 	public void findOrNodesBelow(OrNode orNode, LinkedList<OrNode> below) {
 		// find the child of order
